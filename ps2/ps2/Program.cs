dcfa0437cb75d9f3e5897ebaa8a719f926284965
LiSa <Liyasfox2580@gmail.com>
2022-10-04 10:38:31 +0300

    Добавьте файлы проекта.

---
+++ ps2/ps2/Program.cs
@@ -0,0 +1,14 @@
+﻿double k, e, p, step, mult;
+k = 1;
+e = 0.1;
+p = 1;
+mult = 1;
+while ((Math.Abs(Math.PI - p)) > e)
+{
+    step = k * k;
+    mult *= (4 * step) / (4 * step - 1);
+    p = 2 * mult;
+    k++;
+}
+Console.WriteLine(p);
+Console.WriteLine(Math.Abs(Math.PI - p));
